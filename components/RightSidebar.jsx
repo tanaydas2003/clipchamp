acbbe5811d7b347a8cc616f7950c63ef09378a7a
Tanay Das <111950834+tanaydas2003@users.noreply.github.com>
2025-01-11 19:59:29 +0530

    Update RightSidebar.jsx

--- components/RightSidebar.jsx
+++ components/RightSidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Box, List, ListItem, ListItemText, ListItemIcon } from '@mui/material';
-// import AudioTrackIcon from '@mui/icons-material/AudioTrack';
+import { Box, List, ListItem, ListItemIcon } from '@mui/material';
+import AudioTrackIcon from '@mui/icons-material/AudioTrack';
 import FilterDramaIcon from '@mui/icons-material/FilterDrama';
 import SpeedIcon from '@mui/icons-material/Speed';
 
@@ -8,17 +8,17 @@ const RightSidebar = () => {
   return (
     <Box sx={{ backgroundColor: '#2e2e3e', width: '80px', padding: 2, color: '#fff' }}>
       <List>
-{/*         <ListItem button>
+        <ListItem button={true}> 
           <ListItemIcon>
             <AudioTrackIcon sx={{ color: '#fff' }} />
           </ListItemIcon>
-        </ListItem> */}
-        <ListItem button>
+        </ListItem>
+        <ListItem button={true}>
           <ListItemIcon>
             <FilterDramaIcon sx={{ color: '#fff' }} />
           </ListItemIcon>
         </ListItem>
-        <ListItem button>
+        <ListItem button={true}>
           <ListItemIcon>
             <SpeedIcon sx={{ color: '#fff' }} />
           </ListItemIcon>
